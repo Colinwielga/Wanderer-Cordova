1923fe0d5bce9c4a0503edcfb39e113b6728bc73
DeepwaterCreations <dylan.craine@deepwatercreations.com>
2016-12-03 13:57:36 -0700

    Add missing ;s

--- Wanderer/www/modules/dc-pbta-moves/script.js
+++ Wanderer/www/modules/dc-pbta-moves/script.js
@@ -14,8 +14,8 @@ var component = function () {
     // the communicator also allows know what to have to write also holds the infomation 
     // all events are optional
     this.OnStart = function (communicator,dependencies) {
-        this.communicator = communicator
-        this.Dependencies = dependencies
+        this.communicator = communicator;
+        this.Dependencies = dependencies;
     }
     // called when a new character is created
     this.OnNewCharacter = function () {
