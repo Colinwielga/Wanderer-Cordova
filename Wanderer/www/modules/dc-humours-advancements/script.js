8e75dfec7758eefc921964716adcc34bf6ae2eba
DeepwaterCreations <dylan.craine@deepwatercreations.com>
2017-02-06 21:38:59 -0700

    Go back to advancements being objects
    They live in one of two lists, depending on if they've been taken or
    not, and the "taken" list is the one that gets saved. Now they can also
    have unique properties for things like if they can be taken more than
    once.

--- Wanderer/www/modules/dc-humours-advancements/script.js
+++ Wanderer/www/modules/dc-humours-advancements/script.js
@@ -7,29 +7,75 @@ DCHumoursAdvancements.component = function () {
             humour: "blood",
             label: "Sanguine Advancements",
             expanded: false,
-            pc_advs: {},
-            mc_advs: {}
+            pc_advs: {
+                taken: [
+                    {
+                        text: "The player gets a lovely red canoe"
+                    }
+                ],
+                untaken: [
+                    {
+                        text: "The player feels calm and at peace"
+                    },
+                    {
+                        text: "The player gets a 20% discount at Costco"
+                    }
+                ]
+            },
+            mc_advs: {
+                taken: [
+                    {
+                        text: "The player's favorite stuffed animal loses all of its stuffing"
+                    },
+                    {
+                        text: "The player is forced to take a bath"
+                    }
+                ],
+                untaken: [
+                    {
+                        text: "The player's luggage is sent to Belgium"
+                    }
+                ]
+            }
         },
         {
             humour: "yellowbile",
             label: "Choleric Advancements",
             expanded: false,
-            pc_advs: {},
-            mc_advs: {}
+            pc_advs: {
+                taken: [],
+                untaken: []
+            },
+            mc_advs: {
+                taken: [],
+                untaken: []
+            }
         },
         {
             humour: "blackbile",
             label: "Melancholic Advancements",
             expanded: false,
-            pc_advs: {},
-            mc_advs: {}
+            pc_advs: {
+                taken: [],
+                untaken: []
+            },
+            mc_advs: {
+                taken: [],
+                untaken: []
+            }
         },
         {
             humour: "phlegm",
             label: "Phlegmatic Advancements",
             expanded: false,
-            pc_advs: {},
-            mc_advs: {}
+            pc_advs: {
+                taken: [],
+                untaken: []
+            },
+            mc_advs: {
+                taken: [],
+                untaken: []
+            }
         }
     ];
     
@@ -44,8 +90,13 @@ DCHumoursAdvancements.component = function () {
         return advs;
     };
 
-    this.toggleTaken = function(adv_dict, adv){
-        adv_dict[adv] = !adv_dict[adv];
+    this.take = function(adv_dict, adv){
+        adv_dict.untaken.splice(adv_dict.untaken.indexOf(adv), 1);
+        adv_dict.taken.push(adv);
+    }
+    this.untake = function(adv_dict, adv){
+        adv_dict.taken.splice(adv_dict.taken.indexOf(adv), 1);
+        adv_dict.untaken.push(adv);
     }
 
     //Expands or collapses the list in the view
