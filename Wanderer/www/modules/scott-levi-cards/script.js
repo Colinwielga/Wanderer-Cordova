3373723304298c0b556752702b59f30a05f49fd6
SeigneurF <349228hertz@gmail.com>
2018-05-19 11:49:38 -0600

    printing listed abilities

--- Wanderer/www/modules/scott-levi-cards/script.js
+++ Wanderer/www/modules/scott-levi-cards/script.js
@@ -117,7 +117,49 @@ ScottLeviCards.component = function () {
     
         }        
         // todo a whole lot of code goes here!
-        return ["Wands; " + numWands, "Cups; " + numCups, "Swords; " + numSwords, "Pentacles; " + numPentacles];
+        var WandsAbilities = ['Elements, control a palm-sized amount of your chosen element double the size for succeeding poker hand power levels.', 
+            'Telepathy, mental interference and protection from psychic attacks, higher discards allow temporary mind control.', 
+            'Matter Manipulation, alchemy and chemistry, growing/shrinking, invisibility.', 
+            'Time Manipulation; temporarily change one PC or NPC’s perception of time.',
+            'Mastery, Aces now count as higher value than Kings and; Discard any Wand, draw a card; if it’s the Page draw 3 cards.',
+            'Page of Wands: discard a Pair, interrupt enemy magickal attacks, break an on going enemy spell.',
+            'Knight of Wands: discard Two Pairs, and discard any Prime numbered card; automatically pass the next Magick related skill check.',
+            'Queen of Wands: You may help other Players pass checks, by discarding your own cards. Discard one card; if the person you are assisting fails their check, your hand size decreases by one.',
+            'King of Wands: Discard Three of a Kind, cast any Magick Spell you can think of, work with the DM to determine something fair and flavorful.  (Stuff like like: Time Reversal, Go Below Absolute Zero…)',
+            ]
+        var SwordsAbilities = ['Close Quarters, armed melee attacks (daggers, shivs, etc.)', 
+            'Mid Range, across the room attacks (throwing knives, hand guns, etc.)', 
+            'Long Range, across the block attacks (sniper rifle, laser, missile, etc.)',
+            'Area of Effect, blast radius and multi-target attacks (grenades, flechettes, etc.)', 
+            'Mastery, three card flushes can count towards skill checks and; Discard any Sword card, draw a card; if it’s the Page draw 3 cards.',
+            'Page of Swords: discard a Pair, make a Double Attack (same move twice or two different weapons).', 
+            'Knight of Swords: discard Two Pairs, make a Triple Attack.',
+            'Queen of Swords: You may help other Players pass checks. Discard one card; if the person you are assisting fails their check, your hand size decreases by one.', 
+            'King of Swords: Discard Three of a Kind, gain an additional Shield layer; when your Encounter HP hits 0, reset it to full one time per combat.',
+            ]
+        var PentaclesAbilites = ['Botany, Any Character can eat flowers for power ups, but only Botanists know which plants will be beneficial and how to craft their own plant power ups for personal and party needs.  The number of tricks taken by botanist player determines number of positive effects; the DM’s tricks represent negative side effects. If the other Players involved take any tricks they may add their own qualities.',   
+            'Mobility – Piloting ground or space vehicles and advanced power armor acrobatics.',
+            'Builder – Crafting technology.  Number of tricks taken in mini Hearts corresponds to what you can make.  How strong, how many, and how fast will be decided by the usual skill check resolution.  One trick, a wall/ramp/platform.  Two tricks, a weapon.  Three tricks, a pentacle/magick channeling item.  Four tricks, a new suit of armor.  Five tricks, a new scroll.  Six tricks, a vehicle.  Seven tricks, Whatever you want within reason to the DM and Party.',
+            'Enhance Speed - give yourself or an ally character a speed boost. This counters/is countered by Wands Time Manipulation.',
+            'Mastery, three card flushes can count towards skill checks and; Discard a Pentacle card, draw a card; if it’s the Page draw 3 cards.',
+            'Page of Pentacles: Discard a Pair, make minor repairs to any technology.', 
+            'Knight of Pentacles: Discard Two Pairs, make major repairs to any technology.', 
+            'Queen of Pentacles: You may help other Players pass checks. Discard one card; if the person you are assisting fails their check, your hand size decreases by one.',
+            'King of Pentacles: Discard Three of a Kind, must be Pentacle suited; your power armor suit can perpetually fly.  Discard Three of a Kind nonPentacles, gain the power of flight for the duration of an encounter.',
+            ]
+        var CupsAbilities = ['Perception; sizing up a situation, nonviolent conflict resolution.', 
+            'Coin; participating in the blockchain currency called Coin for any financial transactions.',  
+            'Energy; command a vast energy source for a specific task, like recharging a shield battery, or rebooting a scroll, etc. Sources of energy can be Spiritual, solar, mechanical, etc.',
+            'Advanced Linking; transform into a combination of Avatars with one of your fellow players or an NPC.  Players are in charge of using their own abilities and weapons. Each must agree which specific functions they have control over.  If the player is with an NPC, they will have full control of the combined suit.', 
+            'Mastery; The Full Voltron, as long as your character is involved, you may fully combine with your entire party, but no more than that number at any given time.  Same rules as at Level 4, but now with more than 2 characters or players involved (three suits total) and; Discard a Pentacle card, draw a card; if it’s the Page draw 3 cards.',
+            'Page of Cups: Discard a Pair, Saboteur, while doing something unexpected or from concealment, you can make a rogue-like sneak attack or action that sabotages anyone or something in the environment.', 
+            'Knight of Cups: Discard Two Pairs, Face Cards (Page-King) outrank and beat Wild and Major Arcana for the current round of combat/action.', 
+            'Queen of Cups: You may help other Players pass checks. Discard one card; if the person you are assisting fails their check, your hand size decreases by one.',  
+            'King of Cups: You may only advance to this level if you have the seven (chariot) and thirteen (devil) In Play.  Discard Three of a Kind, Cup suited face Cards count as Wild for your next skill check.',
+            ]
+            
+        return ["Wands; " + numWands + ' - ' + WandsAbilities[numWands], "Cups; " + numCups+ ' - ' + CupsAbilities[numCups], "Swords; " + numSwords + ' - ' + SwordsAbilities[numSwords], "Pentacles; " + numPentacles+ ' - ' + PentaclesAbilities[numPentacles]];
+        
     } 
 
     this.Dragging = function() {
@@ -287,6 +329,7 @@ ScottLeviCards.component = function () {
             }
         }
         for (var i = 0; i < this.inPlay.length; i++) {
+        for (var i = 0; i < this.hand.length; i++) {
             if (this.inPlay[i] === cardID) {
                 this.inPlay.splice(i, 1);
             }
