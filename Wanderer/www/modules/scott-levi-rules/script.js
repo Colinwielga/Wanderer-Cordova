1fa3d7c7a99e13f135d675aa37f84ae8d029df39
Colin <ColinWielga@gmail.com>
2018-05-22 19:59:35 -0400

    rules

---
+++ Wanderer/www/modules/scott-levi-rules/script.js
@@ -0,0 +1,54 @@
+﻿var ScottLeviRules = {};
+
+ScottLeviRules.component = function () {
+
+    this.getId = function () {
+        return "scott-levi-rules"
+    }
+
+    this.OnStart = function (communicator, logger, page,dependencies) {
+        this.communicator = communicator
+        this.Dependencies = dependencies
+    }
+
+    this.OnNewCharacter = function () { }
+
+    this.OnSave = function () { }
+
+    this.OnLoad = function () { }
+
+    this.OnUpdate = function () {
+    }
+    
+    this.getRequires = function () {
+        return [];
+    }
+
+    this.getPublic = function () {
+        return {
+            getVersion: function () {
+                return 1;
+            }
+        }
+    }
+
+    this.canClose = function () {
+        return true;
+    }
+
+    this.getHmtl = function () {
+        return "modules/" + this.getId() + "/page.html"
+    }
+
+    this.getRulesHtml = function () {
+        return "modules/" + this.getId() + "/rules.html"
+    }
+
+    this.getTitle = function () {
+        return "Fools RPG Rules";
+    }
+
+    this.OnNewCharacter();
+}
+
+g.services.componetService.registerCharacter(ScottLeviRules.component);
