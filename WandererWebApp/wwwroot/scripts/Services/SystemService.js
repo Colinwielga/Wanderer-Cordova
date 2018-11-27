d565d5181527ed249787c8f67547ed8eb5373edc
Prototypist1 <ColinWielga@outlook.com>
2018-11-26 21:43:30 -0500

    here is some stuff

---
+++ WandererWebApp/wwwroot/scripts/Services/SystemService.js
@@ -0,0 +1,20 @@
+﻿g.services.SystemService = {
+    // pass takes an account object
+    // fail takes an error
+    GetSystem: function ( accessKey, good, doesNotExist, fail) {
+        g.services.AWSConnector.GetSystem(
+            accessKey,
+            good,
+            doesNotExist,
+            fail);
+    },
+    SaveSystem: function (accessKey,name,json,good,doesNotExist, fail) {
+        g.services.AWSConnector.SaveSystem(
+                accessKey,
+                name,
+                json,
+                good,
+                doesNotExist,
+                fail);
+    }
+};
