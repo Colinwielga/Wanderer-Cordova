cc6a0ace0bd886a8925a870e5743b333113ed67e
Prototypist1 <ColinWielga@outlook.com>
2019-01-19 12:37:02 -0500

    more work on table sync

--- WandererWebApp/wwwroot/scripts/Pages/Table/TableController.js
+++ WandererWebApp/wwwroot/scripts/Pages/Table/TableController.js
@@ -28,7 +28,7 @@
 
                 // for now we will just log
                 // run it!
-                //console.log("got a message:", message);
+                console.log("got a message:", message);
 
                 // check if the message is a move
                 if (message.message === "miniature moved") {
@@ -36,7 +36,7 @@
                     // acces the right miniature and update that
 
                     for (let miniature of allMiniatures) {
-                        if (miniature.text === message.text) {
+                        if (miniature.miniatureId === message.miniatureId) {
                             miniature.realX = message.locationX;
                             miniature.realY = message.locationY;
                         }
@@ -49,28 +49,30 @@
                     var isNewToUs = true;
 
                     for (let miniature of allMiniatures) {
-                        if (miniature.text === message.text) {
+                        if (miniature.miniatureId === message.miniatureId) {
                             isNewToUs = false;
                         }
                     }
 
                     if (isNewToUs) {
-                        createMiniature(message.text, message.occupation, message.locationX, message.locationY, message.img, false)
+                        createMiniature(message.miniatureId, message.text, message.occupation, message.locationX, message.locationY, message.img, false);
                     }
                 }
             });
         });
 
-    var createMiniature = function (name, occupation, xPosition, yPosition, img, sendMessage) {
+    var createMiniature = function (miniatureId,name, occupation, xPosition, yPosition, img, sendMessage) {
         var miniature = {
+            miniatureId: miniatureId,
             text: name,
             occupation: occupation,
             realX: xPosition,
             realY: yPosition,
             img: img,
+            lastSent: 0,
             x: function () { return this.realX + "px"; },
             y: function () { return this.realY + "px"; },
-            onDragComplete: function (data, event) {
+            move: function (data, event,alwaysSend) {
 
                 var currentX = event.originalEvent.clientX; 
                 var currentY = event.originalEvent.clientY; 
@@ -84,21 +86,27 @@
                 this.realX = this.realX + moveX;
                 this.realY = this.realY + moveY;
 
+                var now = (new Date).getTime();
 
-                // TODO send a message to tell the world we moved a miniature
-
-                g.services.SingnalRService.Send(key,
-                    {
-                        module: "table",
-                        message: "miniature moved",
-                        text: name,
-                        locationX: this.realX,
-                        locationY: this.realY,
-                        SendBy: id
-                    });
+                if (now - this.lastSent > 100 || alwaysSend){
+                    g.services.SingnalRService.Send(key,
+                        {
+                            miniatureId: this.miniatureId,
+                            module: "table",
+                            message: "miniature moved",
+                            text: name,
+                            locationX: this.realX,
+                            locationY: this.realY,
+                            SendBy: id
+                        });
+                    this.lastSent = now;
+                }
+            },
+            onDragComplete: function (data, event) {
+                this.move(data, event,true);
             },
             onDragMove: function (data, event) {
-                this.onDragComplete(data, event);
+                this.move(data, event,false);
             },
             onDragStart: function (data, event) {
                 this.lastX = event.originalEvent.clientX;// number 100
@@ -106,11 +114,12 @@
             }
         };
 
-        allMiniatures.push(miniature)// add mini to our lsit
+        allMiniatures.push(miniature);
 
         if (sendMessage) {
             g.services.SingnalRService.Send(key,
                 {
+                    miniatureId: miniature.miniatureId,
                     module: "table",
                     message: "miniature created",
                     text: name,
@@ -123,12 +132,12 @@
         }
     };
     
-    createMiniature("hal 9000", "Computer", 100, 100, "images/cards/0.jpg", false);
-    createMiniature("spock", "Science Officer", 200, 200, "images/cards/1.jpg", false);
-    createMiniature("yoda", "Jedi Master", 666, 333, "images/cards/2.jpg", false);
-    createMiniature("mace windu", "Jedi Master", 333, 666, "images/cards/3.jpg", false);
-    createMiniature("prince", "Musician", 250, 750, "images/cards/4.jpg", false);
-    createMiniature("einstein", "Physicist", 750, 250, "images/cards/5.jpg", false);
+    createMiniature("362834729","hal 9000", "Computer", 100, 100, "images/cards/0.jpg", false);
+    createMiniature("758341938","spock", "Science Officer", 200, 200, "images/cards/1.jpg", false);
+    createMiniature("789519764","yoda", "Jedi Master", 666, 333, "images/cards/2.jpg", false);
+    createMiniature("249751635","mace windu", "Jedi Master", 333, 666, "images/cards/3.jpg", false);
+    createMiniature("824691375","prince", "Musician", 250, 750, "images/cards/4.jpg", false);
+    createMiniature("548547623","einstein", "Physicist", 750, 250, "images/cards/5.jpg", false);
     
     
     
