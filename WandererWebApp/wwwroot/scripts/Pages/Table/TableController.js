494e83caa8c49702a0d814a4140de7e29b724bf4
s.L <349228hertz@gmail.com>
2019-04-01 19:11:32 -0600

    Is this what we did?

--- WandererWebApp/wwwroot/scripts/Pages/Table/TableController.js
+++ WandererWebApp/wwwroot/scripts/Pages/Table/TableController.js
@@ -1,6 +1,6 @@
 ﻿g.getTableController = function ($timeout, message) {
 
-    let allStrokes = [];    
+    let allStrokes = [];
 
     let makeStroke = function (color, startingPoint) {
         // huh it wants the { to be on the same line as the return...
@@ -11,14 +11,14 @@
         // you broke up, what line?
 
         return {
-            color: color, 
-            points: [startingPoint] 
+            color: color,
+            points: [startingPoint]
         };
-    }; 
+    };
 
     let allMiniatures = [];
 
-    let reDraw = function (){
+    let reDraw = function () {
         // 💩 TODO Colin
         // the UI should reach in the controller for values
         // bad form for the controller to reach in to the UI and monkey 🐵
@@ -32,7 +32,7 @@
         canvas.height = hackyHackyHackHack.clientHeight;//4000;//
 
         context.clearRect(0, 0, context.canvas.width, context.canvas.height); // Clears the canvas
-        
+
         context.lineJoin = "round";
         // this is another fun thing to play with!
         // line width
@@ -47,67 +47,29 @@
             context.strokeStyle = stroke.color;
 
             var first = true;
-            //here is something need to change
-            // stroke used to be a list a of points
-            // now stroke is an object
-
-            // you can use 'for (let .. of LIST)'
-            // and it goes through all the items in the list
-            // not so much for an onject
-            // so what we need to do is get the list out of the object
-
-            // any guess on ??
-            //stroke. is a good start
-            // look up at where we create stroke 
-            // what does it have?
-
-            // the names of things stroke has are on the left side of the :
-            // so it has color and points
-            // where you see
-            // "points:[startingPoint]"
-            // that sets the Strokes points property to [startintPoint]
-            // [] is a new list 
-            // [startingPoint] is a list with the item startingPoint in it
-            // so points:[startingPoint]
-            // sets the object's points property to a list of points this is just the starting point
-            
-            // confused?
-
-            // yeah
-            
-            // only two legel things are:
-
-            // stroke.color and stroke.points
-            // stroke only has the stuff on the left of the :
-
-            // I think that is all we have to do
-            
-            // it does not work?
-            // oh so it really does not work
-
 
             var list = stroke.points;
             // I put it there
             for (let point of list) {
-                
+
                 // add points to the path
-                if (first ){
+                if (first) {
                     context.moveTo(point.x, point.y);
                     first = false;
-                }else {
+                } else {
                     context.lineTo(point.x, point.y);
                 }
-                                    
+
             }
             context.stroke();
         }
     };
-    
+
     var id = Math.random() + "";
 
     // toReturn is the controller
     var toReturn = {
-        drawTool:false,
+        drawTool: false,
         eraseTool: false,
         color: "#000000",
         message: message,
@@ -143,7 +105,7 @@
                 Math.random() * 500,
                 {
                     text: this.labelText
-                }, 
+                },
                 false,
                 "scripts/Pages/Table/Label.html");
         },
@@ -165,19 +127,19 @@
             this.drawTool = false;
             reDraw();
         },
-        remove: function (toRemove) { 
+        remove: function (toRemove) {
             var nextList = [];
             for (let miniature of allMiniatures) {
                 if (miniature.miniatureId !== toRemove.miniatureId) {
-                    nextList.push(miniature); 
-                }   
+                    nextList.push(miniature);
+                }
             }
             allMiniatures = nextList;
             // todo publish
         },
         mouseIsDown: false,
         mouseOver: function (event) {
-            if (this.drawTool === true){
+            if (this.drawTool === true) {
                 if (this.mouseIsDown === true) {
                     var stroke = allStrokes[allStrokes.length - 1];
 
@@ -185,14 +147,14 @@
                         x: event.originalEvent.layerX,
                         y: event.originalEvent.layerY
                     };
-                    
+
                     // another one!
                     stroke.points.push(point);
-                    reDraw();    
+                    reDraw();
                 }
             }
         },
-        mouseUp: function(event){
+        mouseUp: function (event) {
             if (this.drawTool === true) {
                 this.mouseIsDown = false;
                 var stroke = allStrokes[allStrokes.length - 1];
@@ -202,20 +164,12 @@
                     y: event.originalEvent.layerY
                 };
 
-                // here!
-                // here!
-                // same problem
-                // stroke is not a list 
-                // we can not push it 
-                // stroke is an object
-                // add the new point to the end
-                //👍
                 stroke.points.push(point);
                 reDraw();
             }
         },
 
-        mouseDown: function(event){
+        mouseDown: function (event) {
             if (this.drawTool === true) {
                 this.mouseIsDown = true;
 
@@ -223,66 +177,13 @@
                     x: event.originalEvent.layerX,
                     y: event.originalEvent.layerY
                 };
-                
-                // this looks good to me
-                // we need to make a few more changes
-                // since we changed how strokes work
-                // we have to update everything that was using strokes
-                // haha
-                // so the big thing that was using stroke is reDraw
-                // so let's update that
-
-                // one more issue!
-                // I just found
-                // I am just going to fix it
-                // it is a stupid this
-                // try now
-                // we need to get the color off the encolsing object
-                // I will try to explain
-                // but I think it really requires a full lecture
-                // awww yeah!
-
-                // so this...
-
-                // sometimes you make and object
-                // and the object needs to refer to it self
-                // when it needs to do that it uses 'this'
-                // 
-                let x =0;
-                let object = {
-                    x:5,
-                    getX:function(){
-                        //return x;// if we just write x we get 0
-                        return this.x // if we write this.x we 5
-                        // it is crazy
-                        // a little crazy
-
-                    }
-                }
-
 
                 var stroke = makeStroke(this.color, point);
                 allStrokes.push(stroke);
 
-                // I was in the wrong visual studio
-                //
-
-                // so this line is not so good
-                // stroke used to be a list
-                // so stroke.push made a lot of sense
-                // push adds to the end of the list
-                // but now not so much 
-                // raised eyebrow emoji
-                // stroke.
-                // right we get the list of points the stroke has
-                // and we add the new point to them
-
-                // we could also write this in the more compact:
-                // 👍
-                // yeah just simplifed it a little
                 stroke.points.push(point);
                 reDraw();
-                
+
             }
         }
     };
@@ -362,15 +263,15 @@
 
                 this.realX = this.realX + moveX;
                 this.realY = this.realY + moveY;
-                
+
                 if (rounded) {
                     this.realX = Math.round(this.realX / 100) * 100;
                     this.realY = Math.round(this.realY / 100) * 100;
                 }
-                
+
                 var now = (new Date).getTime();
 
-                if (now - this.lastSent > 100 || alwaysSend){
+                if (now - this.lastSent > 100 || alwaysSend) {
                     g.services.SingnalRService.Send(key,
                         {
                             miniatureId: this.miniatureId,
