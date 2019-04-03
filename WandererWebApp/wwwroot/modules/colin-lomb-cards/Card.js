2ffa2bed781fc18844777febc4d5126843bb0705
Prototypist1 <ColinWielga@outlook.com>
2019-04-03 18:48:58 -0400

    bones of lomb hand

--- WandererWebApp/wwwroot/modules/colin-lomb-cards/Card.js
+++
@@ -1,6 +0,0 @@
-﻿ColinLombCards.Card = function (text, value) {
-    this.text = text;
-    this.value = value;
-    this.html =  "modules/colin-lomb-cards/card.html";
-};
-
--- WandererWebApp/wwwroot/modules/colin-lomb-cards/script.js
+++ WandererWebApp/wwwroot/modules/colin-lomb-cards/script.js
@@ -1,34 +1,37 @@
-﻿ColinLombCards.component = function () {
-    var that = this;
-    this.decklist = ColinWielgaCards.decklist;
+﻿let ColinLombCards = {};
+
+ColinLombCards.Card = function (text, value) {
+    return {
+        text: text,
+        value: value
+    };
+};
+
+ColinLombCards.component = function () {
+
     this.getId = function () {
         return "colin-lomb-cards";
     };
+
     this.OnStart = function (communicator, logger, page, dependencies) {
         this.communicator = communicator;
         this.godsPublic = dependencies[0];
     };
+
     this.OnNewCharacter = function () {
         this.hand = [];
-        this.selectedDeck = ColinWielgaCards.decklist[0];
-        this.activeDeck = this.selectedDeck.defaultActive();
     };
+
     this.OnSave = function () {
         this.communicator.write("hand", this.hand);
     };
+
     this.OnLoad = function () {
         var version = this.communicator.lastVersion();
         this.OnNewCharacter();
-        if (version === 1.0) {
-            // now we have selected a deck
-            // we need the active cards
+        if (version === 1) {
             if (this.communicator.canRead("hand")) {
-                var nextHand = this.communicator.read("hand");
-                nextHand.forEach(function (id) {
-                    if (that.selectedDeck.allCards.hasOwnProperty(id)) {
-                        that.hand.push(id);
-                    }
-                });
+                this.hand = this.communicator.read("hand");
             }
         }
     };
@@ -36,15 +39,19 @@
     this.getHmtl = function () {
         return "modules/" + this.getId() + "/page.html";
     };
+
     this.getRulesHtml = function () {
         return "modules/" + this.getId() + "/rules.html";
     };
+
     this.canClose = function () {
         return true;
     };
+
     this.getTitle = function () {
         return "Lomb Hand";
     };
+
     this.getRequires = function () {
         return ["colin-lomb-alignment"];
     };
@@ -57,34 +64,17 @@
         };
     };
 
-    this.getCard = function (id) {
-        return this.selectedDeck.allCards[id];
+    this.draw = function () {
+        this.hand.push(ColinLombCards.Card("drawn", 3));
     };
 
-    this.draw = function () {
-        if (this.hand.length < this.activeDeck.length) {
-            var num = -1;
-            var fail = false;
-            while (num === -1 || fail) {
-                num = this.activeDeck[Math.floor(Math.random() * this.activeDeck.length)];
-                fail = false;
-                for (var i = 0; i < this.hand.length; i++) {
-                    if (this.hand[i] === num) {
-                        fail = true;
-                        break;
-                    }
-                }
-            }
-            this.hand.push(num);
+    this.discard = function (card) {
+        var index = this.hand.indexOf(card);
+        if (index > -1) {
+            this.hand.splice(index, 1);
         }
     };
-    this.discard = function (cardID) {
-        for (var i = 0; i < this.hand.length; i++) {
-            if (this.hand[i] === cardID) {
-                this.hand.splice(i, 1);
-            }
-        }
-    };
+
     this.OnNewCharacter();
 };
 
