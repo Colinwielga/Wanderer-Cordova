5097a89bc870719eb46e3e049c5d680f90d9cee3
s.L <349228hertz@gmail.com>
2019-07-16 12:34:41 -0600

    Ledger module

---
+++ WandererWebApp/wwwroot/modules/wanderer-core-ledger/script.js
@@ -0,0 +1,42 @@
+﻿var component = function () {
+
+    this.getId = function () {
+        return "wanderer-core-ledger";
+    };
+
+    this.OnStart = function (communicator, logger, page, dependencies) {};
+    this.OnNewCharacter = function () {
+        var message = {};
+        message.sender = "Colin";
+        message.timestamp = "12:45";
+        message.text = "Hello World!";
+        this.messages = [message]; 
+    };
+    this.OnSave = function () {};
+    this.OnLoad = function () {};
+    this.getRequires = function () {
+        return [];
+    };
+    this.getPublic = function () {
+        var that = this;
+        return {
+            getVersion: function () {
+                return 1;
+            }
+        };
+    };
+    this.getHmtl = function () {
+        return "modules/" + this.getId() + "/page.html";
+    };
+    this.getRulesHtml = function () {
+        return "modules/" + this.getId() + "/rules.html";
+    };
+    this.canClose = function () {
+        return true;
+    };
+    this.getTitle = function () {
+        return "Ledger";
+    };
+};
+
+g.services.componetService.registerCharacter(component);
