0f9368597c054923e95f91ce20761b14781d549d
Prototypist1 <ColinWielga@outlook.com>
2020-05-17 11:09:41 -0400

    added shared entity

---
+++ WandererWebApp/ItemCache.cs
@@ -0,0 +1,86 @@
+﻿using Microsoft.Azure.Cosmos.Table;
+using Microsoft.Extensions.Configuration;
+using Newtonsoft.Json.Linq;
+using Prototypist.TaskChain;
+using Prototypist.Toolbox.Object;
+using System;
+using System.Threading.Tasks;
+
+namespace WandererWebApp
+{
+    public class ItemCache
+    {
+
+        private readonly Task<CloudTable> table;
+
+        public ItemCache(IConfiguration config)
+        {
+            table = Storage.CreateTableAsync(config.GetConnectionString("wanderer-table-storage"), "SharedEntities");
+        }
+
+        private MonsterIndexBackedIndex.View<(string, string), Task<JumpBallConcurrent<JObject>>> cache = new MonsterIndexBackedIndex.View<(string, string), Task<JumpBallConcurrent<JObject>>>();
+
+
+        public async Task<string> Do(string entityName, string entityOwner, Func<JObject, JObject> modify) {
+            if (entityName is null)
+            {
+                throw new ArgumentNullException(nameof(entityName));
+            }
+
+            if (entityOwner is null)
+            {
+                throw new ArgumentNullException(nameof(entityOwner));
+            }
+
+            var mine = new TaskCompletionSource<JumpBallConcurrent<JObject>>();
+            var current = cache.GetOrAdd((entityName, entityOwner), mine.Task);
+            if (ReferenceEquals(mine.Task, current))
+            {
+                var retrieveOperation = TableOperation.Retrieve<Entity>(entityName, entityOwner);
+                var result = await (await table).ExecuteAsync(retrieveOperation);
+                var entity = result.Result.SafeCastTo<object, Entity>();
+                if (entity == null) {
+                    entity = new Entity(entityName, entityOwner)
+                    {
+                        JSON = new JObject().ToString(),
+                    };
+                    entity = (await (await table).ExecuteAsync(TableOperation.Insert(entity))).SafeCastTo<object, Entity>();
+                }
+                // TODO get from db
+                // or put in db
+                mine.SetResult(new JumpBallConcurrent<JObject>(JObject.Parse(entity.JSON)));
+            }
+            var jumpBall = await current;
+
+            var jobjectResult = await jumpBall.RunAsync(async jobject =>
+            {
+                jobject = modify(jobject);
+
+                var result = await (await table).ExecuteAsync(TableOperation.InsertOrReplace(new Entity(entityName, entityOwner)
+                {
+                    JSON = jobject.ToString(),
+                }));
+
+                return JObject.Parse(result.Result.SafeCastTo<object, Entity>().JSON);
+            });
+
+            return jobjectResult.ToString();
+        }
+    }
+
+    public class Entity : TableEntity
+    {
+        /// <summary>
+        /// must expose a parameter-less constructor
+        /// </summary>
+        public Entity()
+        {
+        }
+        public Entity(string entityName, string entityOwner)
+        {
+            PartitionKey = entityOwner;
+            RowKey = entityName;
+        }
+        public string JSON { get; set; }
+    }
+}
---
+++ WandererWebApp/Storage.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.Azure.Cosmos.Table;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WandererWebApp
+{
+    public class Storage
+    {
+        /// <summary>
+        /// Validate the connection string information in app.config and throws an exception if it looks like 
+        /// the user hasn't updated this to valid values. 
+        /// </summary>
+        /// <param name="storageConnectionString">Connection string for the storage service or the emulator</param>
+        /// <returns>CloudStorageAccount object</returns>
+        public static CloudTableClient CreateStorageAccountFromConnectionString(string storageConnectionString)
+        {
+            return CloudStorageAccount.Parse(storageConnectionString).CreateCloudTableClient();
+        }
+
+        /// <summary>
+        /// Create a table for the sample application to process messages in. 
+        /// </summary>
+        /// <returns>A CloudTable object</returns>
+        public static async Task<CloudTable> CreateTableAsync(string storageConnectionString, string tableName)
+        {
+            var tableClient = CreateStorageAccountFromConnectionString(storageConnectionString);
+
+            CloudTable table = tableClient.GetTableReference(tableName);
+
+            await table.CreateIfNotExistsAsync();
+
+            return table;
+        }
+    }
+}
