4b1f0bf2794ca8218946d8588c69a0ba64e4cf64
Prototypist1 <ColinWielga@outlook.com>
2020-06-09 18:40:42 -0400

    debounced database saving

--- WandererWebApp/ItemCache.cs
+++ WandererWebApp/ItemCache.cs
@@ -18,12 +18,80 @@ namespace WandererWebApp
             table = Storage.CreateTableAsync(config.GetConnectionString("wanderer-table-storage"), "SharedEntities");
         }
 
-        private MonsterIndexBackedIndex.View<(string, string), Task<JumpBallConcurrent<JObject>>> cache = new MonsterIndexBackedIndex.View<(string, string), Task<JumpBallConcurrent<JObject>>>();
+        private class DataToSave
+        {
+            string entityName;
+            string entityOwner;
+            bool dirty;
+            DateTime lastUpdate;
+            JObject jObject;
+            ItemCache itemCache;
+
+            public DataToSave(JObject jObject, ItemCache itemCache)
+            {
+                this.jObject = jObject;
+            }
+
+            internal async Task SaveIfNeeded(CloudTable table)
+            {
+                if (dirty && DateTime.UtcNow - lastUpdate > TimeSpan.FromMinutes(1)) {
+                    // save
+                     await table.ExecuteAsync(TableOperation.InsertOrReplace(new Entity(entityName, entityOwner)
+                    {
+                        JSON = jObject.ToString(),
+                    }));
+
+                    dirty = false;
+                }
+            }
+
+            // assumes this is the start of this list
+            // it is weird for it to live in here but we need checking dirty and removing to be a transaction
+            internal bool TryRemove() {
+                if (!dirty)
+                {
+                    itemCache.ToSaves.RemoveStart();
+                    return true;
+                }
+                return false;
+            }
+
+            internal void Update(Func<JObject,JObject> update, JumpBallConcurrent<DataToSave> self) {
+                this.jObject = update(jObject);
+                if (!dirty) {
+                    itemCache.ToSaves.Add(self);
+                }
+                dirty = true;
+                lastUpdate = DateTime.UtcNow;
+            }
+        }
+
+        private ConcurrentLinkedList<JumpBallConcurrent<DataToSave>> ToSaves = new ConcurrentLinkedList<JumpBallConcurrent<DataToSave>>();
+
+
+        private MonsterIndexBackedIndex.View<(string, string), Task<JumpBallConcurrent<DataToSave>>> cache = new MonsterIndexBackedIndex.View<(string, string), Task<JumpBallConcurrent<DataToSave>>>();
 
         public async Task<string> Get(string entityName, string entityOwner) {
             var jumpBall = await PrivateGet(entityName, entityOwner);
             var jobjectResult = jumpBall.Read();
             return jobjectResult.ToString();
+        }
+
+        public async Task PassoverToSave() {
+            var readyTable = await table;
+
+            foreach (var toSave in ToSaves)
+            {
+                await toSave.RunAsync(async x=> { await x.SaveIfNeeded(readyTable); return x; });
+            }
+            var go = true;
+            while (go && ToSaves.TryGetFirst(out var dataToSave)) {
+                dataToSave.Run(x =>
+                {
+                    go = x.TryRemove();
+                    return x;
+                });
+            }
         }
 
         public async Task<string> Do(string entityName, string entityOwner, Func<JObject, JObject> modify)
@@ -32,22 +100,23 @@ namespace WandererWebApp
 
             var jumpBall = await PrivateGet(entityName, entityOwner);
 
-            var jobjectResult = await jumpBall.RunAsync(async jobject =>
+            var jobjectResult = await jumpBall.RunAsync(async dts =>
             {
-                jobject = modify(jobject);
+                JObject res;
+                dts.Update(x=> {
 
-                var result = await (await table).ExecuteAsync(TableOperation.InsertOrReplace(new Entity(entityName, entityOwner)
-                {
-                    JSON = jobject.ToString(),
-                }));
+                    res = (JObject)modify(x).DeepClone();
+                    return res;
 
-                return JObject.Parse(result.Result.SafeCastTo<object, Entity>().JSON);
+                    }, jumpBall);
+
+                return dts;
             });
 
             return jobjectResult.ToString();
         }
 
-        private async Task<JumpBallConcurrent<JObject>> PrivateGet(string entityName, string entityOwner)
+        private async Task<JumpBallConcurrent<DataToSave>> PrivateGet(string entityName, string entityOwner)
         {
             if (entityName is null)
             {
@@ -59,7 +128,7 @@ namespace WandererWebApp
                 throw new ArgumentNullException(nameof(entityOwner));
             }
 
-            var mine = new TaskCompletionSource<JumpBallConcurrent<JObject>>();
+            var mine = new TaskCompletionSource<JumpBallConcurrent<DataToSave>>();
             var current = cache.GetOrAdd((entityName, entityOwner), mine.Task);
             if (ReferenceEquals(mine.Task, current))
             {
@@ -76,11 +145,12 @@ namespace WandererWebApp
                 }
                 // TODO get from db
                 // or put in db
-                mine.SetResult(new JumpBallConcurrent<JObject>(JObject.Parse(entity.JSON)));
+                mine.SetResult(new JumpBallConcurrent<DataToSave>(new DataToSave(JObject.Parse(entity.JSON),this)));
             }
             var jumpBall = await current;
             return jumpBall;
         }
+
     }
 
     public class Entity : TableEntity
