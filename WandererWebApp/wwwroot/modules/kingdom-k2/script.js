b9f863f5ec36b07bb6bc84632437470f44574ec3
s.L <349228hertz@gmail.com>
2020-06-15 21:21:58 -0600

    K2 Voting System
    Co-authored-by: Colin Wielga <ColinWielga@gmail.com>

--- WandererWebApp/wwwroot/modules/kingdom-k2/script.js
+++ WandererWebApp/wwwroot/modules/kingdom-k2/script.js
@@ -22,11 +22,11 @@ KingdomK2.component = function () {
         // { playerVotes: [] }
         var playerVotes = this.trackedEntity.SetList("playerVotes");  
         // {playerVotes: [{}]} 
-        var ourPlayer = playerVotes.AppendObject();
+        this.ourPlayer = playerVotes.AppendObject();
         // {playerVotes: [{name:"scott"}]}
-        ourPlayer.SetString("name", "scott");
+        this.ourPlayer.SetString("name", "scott");
         // {playerVotes: [{name:"scott", votes: 100}]}
-        ourPlayer.SetNumber("votes", 100); 
+        this.ourPlayer.SetNumber("votes", 100); 
         // {playerVotes: [{name:"scott", votes: 100},{}]}
         var ourNextPlayer = playerVotes.AppendObject();
         // {playerVotes: [{name:"scott", votes: 100},{name:"colin"}]}
@@ -34,25 +34,50 @@ KingdomK2.component = function () {
         // {playerVotes: [{name:"scott", votes: 100},{name:"colin", votes: 100}}]}
         ourNextPlayer.SetNumber("votes", 100);
 
+        // { activeBills: [] }
+        var activeBills = this.trackedEntity.SetList("activeBills");  
+        // {activeBills: [{}]} 
+        var ourBill = activeBills.AppendObject();
+        // {activeBills: [{name:"declare war on the natives"}]}
+        ourBill.SetString("name", "declare war on the natives");
+        // {activeBills: [{name:"declare war on the natives", opposing:0}]}
+        ourBill.SetNumber("opposing", 0); 
+        // {activeBills: [{name:"declare war on the natives", opposing:0, supporting:0}]}
+        ourBill.SetNumber("supporting", 0);
+        // {activeBills: [{name:"declare war on the natives", opposing:0, supporting:0},{}]}
+        var ourNextBill = activeBills.AppendObject();
+        ourNextBill.SetString("name", "arrest courderoy");
+        ourNextBill.SetNumber("opposing", 0);
+        ourNextBill.SetNumber("supporting", 0);
+        
+        this.proposedBills = this.trackedEntity.SetList("proposedBills");
+        var billProposal = this.proposedBills.AppendObject();
+        billProposal.SetString("name", "ally with the Empire");
+        billProposal.SetNumber("support", 1);
+        var nextProposedBill = this.proposedBills.AppendObject();
+        nextProposedBill.SetString("name", "make momentus king");
+        nextProposedBill.SetNumber("support", 0);
 
-        // goal
-        // {playerVotes: [{name:"scott", votes: 100},{name:"colin", votes: 100}]}
-
-
-        // console.log("tackedEntity",playerVotes.backing[0].backing.name.backing)
+        //  {
+        //      playerVotes: [{name:"scott", votes: 100},{name:"colin", votes: 100}}] 
+        //      activeBills: [{name:"kill the lizard people", supportingVotes: 100, opposingVots: 100},{name:"kill the empire", supportingVotes: 100, opposingVots: 100}]
+        //      proposedBills: [{name:"make monmentous king", support: 100},{name:"make monmentous emperor", support: 100}]
+        //  }
         
 
-        //playerVotes.backing = []
-        
-        // scott 
-        //playerVotes[0].name
-
-        // scott
-        //playerVotes.backing[0].backing.name.backing
-
-        // module.trackedEntity.backing.playerVotes.backing[0].backing.name.backing
-        //
     };
+    
+    this.proposeBill = function () {
+        if (this.ourPlayer.backing.votes.backing > 0) {
+
+            var offerBill = this.proposedBills.AppendObject();
+            offerBill.SetString("name", this.proposedBillText);
+            offerBill.SetNumber("support", 1);
+            this.ourPlayer.backing.votes.Add(-1);
+        }
+    };
+
+    
     this.OnSave = function () {
     };
     this.OnLoad = function () {
@@ -80,7 +105,6 @@ KingdomK2.component = function () {
     this.getRulesHtml = function () {
         return "modules/" + this.getId() + "/rules.html";
     };
-
 
     this.OnNewCharacter();
 };
