296605b9df22babc27e323154222f67d54104829
Prototypist1 <ColinWielga@outlook.com>
2021-02-08 23:28:41 -0500

    updated theme implementation. it is now per-character.

--- WandererWebApp/wwwroot/scripts/Pages/Error/ErrorController.js
+++ WandererWebApp/wwwroot/scripts/Pages/Error/ErrorController.js
@@ -1,6 +1,9 @@
 ﻿g.getErrorController = function ($timeout, message) {
     var toReturn = {
-        message: message
+        message: message,
+        styleClass: function () {
+            return { darkMode: false, funMode: false };
+        }
     };
     return toReturn;
 };
--- WandererWebApp/wwwroot/scripts/Pages/Loading/LoadingController.js
+++ WandererWebApp/wwwroot/scripts/Pages/Loading/LoadingController.js
@@ -1,6 +1,9 @@
 ﻿g.getLoadingController = function (message) {
     var toReturn = {
-        message: message
+        message: message,
+        styleClass: function () {
+            return { darkMode: false, funMode: false };
+        }
     };
     return toReturn;
 };
