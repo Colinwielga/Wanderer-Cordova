a6197517056bbfdeee09b6fa9800947dc090f5ab
s.L. <349228hertz@gmail.com>
2024-04-29 20:21:41 -0600

    Moving around Grid Map

---
+++ WandererWebApp/wwwroot/modules/scott-levi-map-grid/script.js
@@ -0,0 +1,110 @@
+﻿var ScottLeviMapGrid = {};
+
+ScottLeviMapGrid.component = function () {
+    this.grid =[
+        [{description:"coastline"},{description:"coastline"}, {description:"coastline"}, {description:"coastline"},{description:"coastline"},{description:"coastline"},{description:"fishing-village"},{description:"coastline"},{description:"ice-bridge"},{description:"ice-castle"}],
+        [{description:"sea-caves"},{description:"underground-ruins"},{description:"volcanic-mountains"},{description:"geysers"},{description:"blockfield"},{description:"refuge"},{description:""},{description:"ice-bog"},{description:"great-door"},{description:"ice-bridge"}] 
+        [{description:"coastline"},{description:"wreckage"},{description:"cliffs"},{description:"glacier"},{description: "mountains"},{description:"hot-springs"},{description:"frozen-lake"},{description:"basin"},{description:"ice-maze"}, {description:"coastline"},{description: "icy-island"}],
+        [{description:"coastline"},{description:"river"},{description:"mountains"},{description:"glacier"},{description:"arete"},{description:"burial-ground"},{description:"ruins"},{description:"valley"},{description:"burial-ground"},{description:"coastline"}],
+        [{description:"coastline"},{description:"outpost"},{description:"monastary"},{description:"summit"},{description:"glacier"},{description:"waterfall"},{description:"river"},{description:"river"},{description:"river"},{description:"cliffs"},{description:"coastline"}],
+        [{description:"coastline"},{description:"tundra"},{description:"mountains"},{description:"glacier"},{description:"crevasse"},{description:"mineral-desposits"},{description:"foothills"},{description:"permafrost-plateau"},{description:"coastline"}],
+        [{description:"coastline"},{description:"tundra"},{description:"mountains"},{description:"glacier"},{description:"ice-caves"},{description:"foothills"},{description:"temple-complex"},{description:"underground-passages"},{description:"canyons"},{description:"coastline"}],
+        [{description:"coastline"},{description:"forest"},{description:"forest"},{description:"mountains"},{description:"glacier"},{description:"moraine"},{description:"monadnock-mountain"},{description:"forest"},{description:"knoll"},{description:"scree"},{description:"coastline"}],
+        [{description:"coastline"},{description:"forest"},{description:"frozen-farmlands"},{description:"colony"},{description:"mega-project"},{description:"forest"},{description:"ravine"},{description:"gully"},{description:"ruins"},{description:"coastline"}],
+        [{description:"coastline"},{description:"sanctuary"},{description:"frozen-farmlands"},{description:"fjord"},{description:"naval-yard"},{description:"landing-site"},{description:"river"},{description:"meadow"},{description:"meadow"},{description:"coastline"}],
+        [{description:"sunken-continent"}, {description:"coastline"}, {description:"coastline"}, {description:"fjord"},{description:"coastline-bay"},{description:"coastline"},{description:"coastline"},{description:"coastline"},{description:"coastline"},{description:"coastline"}]
+    ]
+    this.x = 2;
+    this.y = 2;
+    this.getVisibleGrid = function () {
+        var result = [];
+        for (var y = 0; y < this.grid.length; y++) {
+            if (this.y - y <= 1 && this.y - y >= -1) {
+                var rowResult = [];
+                for (var x = 0; x < this.grid[y].length; x++) {
+                    if (this.x - x <= 1 && this.x - x >= -1) {
+                        rowResult.push(this.grid[y][x]);    
+                    }
+                }
+                result.push(rowResult);
+            }
+        }
+        return result;
+    }
+    
+    this.getSystem = function () {
+        return "Six Shooter"
+    };
+    // all component need a unique ID
+    this.getId = function () {
+        return "scott-levi-map-grid";
+    };
+
+    // A component should know how to handle some events
+    // called when Wanderer is ready to talk to us
+    // a component talks to the rest of the app throught a communicator
+    // the communicator will call the components methods like OnNewCharacter and OnSave at the appropreat time
+    // the communicator also allows know what to have to write also holds the infomation 
+    // all events are optional
+    this.OnStart = function (communicator, logger, page, dependencies) {
+        this.communicator = communicator;
+        this.Dependencies = dependencies;
+    };
+    // called when a new character is created
+    this.OnNewCharacter = function () {
+        // something like:
+        //this.key = "value";
+    };
+    // called when a character is saved
+    this.OnSave = function () {
+        // something like:
+        //this.communicator.write("key",this.key);
+    };
+    // called when a characrer is loaded 
+    this.OnLoad = function () {
+        // something like:
+        // if (this.communicator.canRead("key")){
+        //this.key = this.communicator.read("key");
+        //}else{
+        //this.key = "default value"
+        //}
+    };
+    this.OnUpdate = function () {
+    };
+
+    // hmm is it really safe for this to be a function?
+    // we use functions so no one can edit
+    this.getRequires = function () {
+        // example of a populated list:
+        // return ["colin-wielga-tools"]
+        return [];
+    };
+
+    this.getPublic = function () {
+        return {
+            getVersion: function () {
+                return 1;
+            }
+        };
+    };
+    // can your module be close?
+    this.canClose = function () {
+        return true;
+    };
+    // a component should be able to provide some infomation
+    this.getHmtl = function () {
+        return "modules/" + this.getId() + "/page.html";
+    };
+
+    this.getRulesHtml = function () {
+        return "modules/" + this.getId() + "/rules.html";
+    };
+
+    this.getTitle = function () {
+        return "Six Shooter Map";
+    };
+
+    this.OnNewCharacter();
+};
+
+g.services.componetService.registerCharacter(ScottLeviMapGrid.component);
---
+++ WandererWebApp/wwwroot/modules/scott-levi-six-shooter/script.js
@@ -0,0 +1,164 @@
+﻿var ScottLeviSixShooter = {};
+
+ScottLeviSixShooter.component = function () {
+    this.actionName=""
+    this.hitPointsCounter = {name: "HP", value: 6.0};
+    this.actionPointsCounter = {name: "AP", value: 6.0};
+    this.confidenceCounter = {name: "confidence", value: 6.0};
+    this.counterList = [this.hitPointsCounter, this.actionPointsCounter, this.confidenceCounter];
+
+    this.simpleAction = {name:"take an action", subActions: [{counterName: "AP", value: -1.0}], subActionCounter: undefined, subActionValue: 0};
+
+    this.injuryAction = {name:"injury", subActions: [{counterName: "HP", value: -1.0}], subActionCounter: undefined, subActionValue: 0};
+    this.loseConfidence = {name:"lose confidence", subActions: [{counterName: "confidence", value: -1.0},], subActionCounter: undefined, subActionValue: 0};
+    this.gainConfidence = {name:"gain confidence", subActions: [{counterName: "confidence", value: +1.0}], subActionCounter: undefined, subActionValue: 0};
+
+    this.actionList = [this.plusHPAction, this.simpleAction, this.injuryAction, this.loseConfidence, this.gainConfidence];
+
+    this.applyAction = function (action) {
+        console.log("applyAction applied")
+        for (var i = 0; i < action.subActions.length; i++){
+            var subAction = action.subActions[i];
+
+            // we need to find the counter we want to modify
+            // so we go through the list and find the one with a matching name
+            for (var j = 0; j < this.counterList.length; j++){
+                var counter = this.counterList[j];
+                
+                // have we found the right one
+                if (subAction.counterName == counter.name){
+                    counter.value = counter.value + subAction.value;
+                };
+            };
+        };
+    };
+
+    this.addSubActionToAction = function (action) { 
+
+        // action is:
+        // - name : string
+        // - subActions : list of sub-actions 
+        // - subActionCounter : counter
+        // - subActionValue : number 
+        // action.subActionCounter is
+        // - name: string
+        // - value: number
+        action.subActions.push({counterName: action.subActionCounter.name, value: action.subActionValue});
+    };
+
+    this.createAction = function (actionName) {
+        this.actionList.push({name: actionName, subActions: []});
+    };
+
+    this.createCounter = function () {
+        this.counterList.push({name: counterName, value: ""})
+    };
+    
+    this.deleteAction = function (action) {
+        for (var i = 0; i < this.actionList.length; i++){
+            var actionListed = this.actionList[i];
+            if (actionListed == action){
+                this.actionList.splice(i, 1);
+            }  
+        }
+    };
+    
+    this.deleteSubAction = function (subAction) {
+        for (var i = 0; i < this.actionList.length; i++){
+            var actionListed = this.actionList[i];
+            for (var j = 0; j < actionListed.subActions.length; j++){
+                var subActionListed = actionListed.subActions[j];
+                if (subActionListed == subAction){
+                    actionListed.subActions.splice(j, 1);
+                }
+            }    
+        }
+    }; 
+
+    this.getId = function () {
+        return "scott-levi-six-shooter";
+    };
+
+    this.OnStart = function (communicator, logger, page, dependencies) {
+        this.communicator = communicator;
+        this.Dependencies = dependencies;
+        this.logger = logger;
+    };
+    
+    this.OnNewCharacter = function () {
+    
+    };
+    
+    this.OnSave = function () {
+        // this.communicator.write("hitPoints", this.hitPoints)
+    };
+     
+    this.OnLoad = function () {
+        var version = this.communicator.lastVersion();
+        this.OnNewCharacter();
+        if (version === 1) {
+            // if (this.communicator.canRead("character moves")) {
+            //     this.characterMoves = this.communicator.read("character moves");
+            // }
+            // if (this.communicator.canRead("minus ability")) {
+            //     this.minusAbility = this.communicator.read("minus ability");
+            // }    
+        };
+    };
+
+    this.OnUpdate = function () {
+    };
+
+    this.getRequires = function () {
+        return ["wanderer-core-ledger"];
+    };
+
+    this.getPublic = function () {
+        return {
+            getVersion: function () {
+                return 1;
+            }
+        };
+    };
+    this.getHmtl = function () {
+        return "modules/scott-levi-six-shooter/page.html"
+    };
+    
+    this.canClose = function () {
+        return true;
+    };
+    
+    this.getHmtl = function () {
+        return "modules/" + this.getId() + "/page.html";
+    };
+
+    this.getRulesHtml = function () {
+        return "modules/" + this.getId() + "/rules.html";
+    };
+
+    this.getTitle = function () {
+        return "Six Shooter";
+    };
+
+    this.getSystem = function () {
+        return "Six Shooter"
+    };
+
+};
+
+g.services.componetService.registerCharacter(ScottLeviSixShooter.component);
+
+//directional choices
+//upstream or downstream
+//uphill or downhill
+//towards a point of interest or away from a point of interest
+
+//name Qwi (for now)
+//supernatural crystals
+//ICS
+
+//crabfolk share a common spoken language, but each tribe has a unique signing and movement based language 
+//nomadic giant milipede riders
+//pearl farmers
+//order of the silver flame
+//
